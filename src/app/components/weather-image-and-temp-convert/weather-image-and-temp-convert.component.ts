a1583a02c710897b0580276a5922398226930ad5
= <=>
2022-03-18 07:01:48 +0000

    added weather image component

--- src/app/components/weather-image-and-temp-convert/weather-image-and-temp-convert.component.ts
+++ src/app/components/weather-image-and-temp-convert/weather-image-and-temp-convert.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, NgIterable, OnInit } from '@angular/core';
+import { Weather } from 'src/app/classes/Weather';
+import { WeatherApiObjects } from 'src/app/classes/WeatherApiObjects';
 
 @Component({
   selector: 'app-weather-image-and-temp-convert',
@@ -8,6 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class WeatherImageAndTempConvertComponent implements OnInit {
 
   constructor() { }
+
+  @Input() weatherDataIcon:Weather[] = [{
+    "id": 800,
+    "main": "Clear",
+    "description": "clear sky",
+    "icon": "01d"
+  }]
+
+  @Input() convertTemp = () =>{}
 
   ngOnInit(): void {
   }
